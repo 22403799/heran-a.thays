111f105ce9d7beae3ef8f2d78fd81635b5c30cfa
22403799 <thaysdf.2014@gmail.com>
2024-11-04 11:05:43 -0300

    Herança no Java - praticando

---
+++ Pessoa.java
@@ -0,0 +1,19 @@
+public class Pessoa {
+protected String nome;
+protected int idade;
+
+// Construtor
+public Pessoa(String nome, int idade) {
+this.nome = nome;
+this.idade = idade;
+
+}
+
+// Método para exibir informações da pessoa
+public void exibirlnfo() {
+System.out.println("Nome: " + nome);
+System.out.println("Idade: " + idade);
+
+}
+
+}
